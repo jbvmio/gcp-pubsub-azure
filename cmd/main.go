1cbc14cb65df9a5847ba2a43b70c60c938d16fbb
jbvmio <jbonds@jbvmio>
2021-08-23 02:11:21 -0400

    add versioning

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,17 +15,26 @@ import (
 var (
 	projectID    string
 	subscription string
+	buildTime    string
+	commitHash   string
 )
 
 func main() {
 	pf := pflag.NewFlagSet(`gcp-pubsub-azure`, pflag.ExitOnError)
 	pf.StringVarP(&projectID, "project", "p", "", "GCP Project ID")
 	pf.StringVarP(&subscription, "subscription", "s", "", "PubSub Subscription")
+	showVer := pf.Bool("version", false, "Show version and exit.")
 	pf.Parse(os.Args[1:])
+	if *showVer {
+		fmt.Printf("Processor : %s\n", "gcp-pubsub-azure")
+		fmt.Printf("Version   : %s\n", buildTime)
+		fmt.Printf("Commit    : %s\n", commitHash)
+		return
+	}
 	logger := internal.ConfigureLogger("info", os.Stdout)
 	defer logger.Sync()
 	L := logger.With(zap.String("process", "gcp-pubsub-azure"))
-	L.Info("starting ...")
+	L.Info("starting", zap.String(`Version`, buildTime), zap.String(`Commit`, commitHash))
 	gcpCreds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	switch {
 	case gcpCreds == "":
